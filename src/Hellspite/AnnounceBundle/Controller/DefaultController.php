f5e089992bc578f99de18178a408fe69214c7c21
Emiliano Nobis <emiliano.nobis@gmail.com>
2012-07-30 21:57:25 +0200

    Permalink per i post

--- src/Hellspite/AnnounceBundle/Controller/DefaultController.php
+++ src/Hellspite/AnnounceBundle/Controller/DefaultController.php
@@ -13,6 +13,20 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getEntityManager();
         $posts = $em->getRepository('HellspiteAnnounceBundle:Post')->getLatest(); 
-        return $this->render('HellspiteAnnounceBundle:Default:index.html.twig', array('posts' => $posts));
+        return $this->render('HellspiteAnnounceBundle:Default:index.html.twig', array('posts' => $posts, 'show' => false));
+    }
+
+    public function showAction($slug){
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('HellspiteAnnounceBundle:Post')->findBySlug($slug);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Post entity.');
+        }
+
+        return $this->render('HellspiteAnnounceBundle:Default:show.html.twig', array(
+            'posts'      => $entity,
+        ));
     }
 }
